deb951d453780a2830921bd4ee24213422fde8dd
_sHaW_ <sivaperumalpriyaraajan6@gmail.com>
2020-02-16 13:32:16 +0530

    Update fire.js

--- src/config/fire.js
+++ src/config/fire.js
@@ -3,14 +3,14 @@ import 'firebase/firestore'
 import 'firebase/storage'
 
 const config = {
-    apiKey: "AIzaSyCppyz4-qIYK3VB5ZPaZT0oOUltcYZU0Mw",
-    authDomain: "react-cheflib.firebaseapp.com",
+    apiKey: "",
+    authDomain: "",
     databaseURL: "https://react-cheflib.firebaseio.com",
     projectId: "react-cheflib",
-    storageBucket: "react-cheflib.appspot.com",
-    messagingSenderId: "16176294304",
-    appId: "1:16176294304:web:97b5f110ea126110b69cc5",
-    measurementId: "G-3YP9LKJ9PF"
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: "
   };
 
   const fire = firebase.initializeApp(config);
